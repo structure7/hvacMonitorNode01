3f8251ebc747354ca63033b766a02a55c723910f
structure7 <kemperm@usa.net>
2016-04-28 00:01:00 -0700

    Alarm notifications.

--- hvacMonitorNode01.ino
+++ hvacMonitorNode01.ino
@@ -1,11 +1,10 @@
 #include <SimpleTimer.h>
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+//#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <TimeLib.h>
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 2  // ESP-01 GPIO 2
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -13,9 +12,11 @@ DallasTemperature sensors(&oneWire);
 DeviceAddress ds18b20house = { 0x28, 0xFF, 0x35, 0x11, 0x01, 0x16, 0x04, 0x25 }; // House temperature probe
 DeviceAddress ds18b20attic = { 0x28, 0xC6, 0x89, 0x1E, 0x00, 0x00, 0x80, 0xAA }; // Attic temperature probe
 
-char auth[] = "fromBlynkApp *04";
+char auth[] = "fromBlynkApp";
 
-int buzzerPin = 0;
+int buzzerPin = 0;  // ESP-01 GPIO 0
+int alarmLatch = 0;
+int alarmTimer;
 
 SimpleTimer timer;
 
@@ -31,7 +32,7 @@ void setup()
   sensors.setResolution(ds18b20attic, 10);
 
   timer.setInterval(2000L, sendTemps); // Temperature sensor polling interval
-  timer.setInterval(2000L, sendStatus); // Maybe alter setInterval to something else so it doesn't hammer the notification app... look at http://playground.arduino.cc/Code/SimpleTimer
+  timer.setInterval(1000L, sendAlarmStatus);
   timer.setInterval(5000L, sendHeartbeat); // Blinks Blynk LED to reflect online status
 }
 
@@ -39,7 +40,7 @@ void sendTemps()
 {
   sensors.requestTemperatures(); // Polls the sensors
   float tempHouse = sensors.getTempF(ds18b20house);
-  float tempAttic = sensors.getTempF(ds18b20attic);  
+  float tempAttic = sensors.getTempF(ds18b20attic);
 
   if (tempHouse >= 30 && tempHouse <= 120)
   {
@@ -57,8 +58,8 @@ void sendTemps()
   else
   {
     Blynk.virtualWrite(7, "ERR");
-  } 
-  led1.off();  
+  }
+  led1.off();
 }
 
 void sendHeartbeat()
@@ -66,15 +67,40 @@ void sendHeartbeat()
   led1.on();
 }
 
-void sendStatus()
+void sendAlarmStatus()
 {
-  if (digitalRead(buzzerPin) == HIGH) // Runs when alarm buzzer is OFF
+  if (digitalRead(buzzerPin) == HIGH) // Runs when alarm buzzer is OFF (pulled high)
   {
-    // Blank (since HIGH == no buzzer with BJT)
+    alarmTimer = 0;
+    alarmLatch = 0;
+    Serial.println("No alarm");
   }
-  else
+  else if (digitalRead(buzzerPin) == LOW && alarmLatch == 0)
   {
-    // SEND NOTIFICATION TO APP
+    Blynk.notify("SECURITY ALARM TRIGGERED.");
+    Serial.println("SECURITY ALARM TRIGGERED.");
+    alarmLatch++;
+  }
+  else if (digitalRead(buzzerPin) == LOW && alarmLatch != 0)
+  {
+    alarmTimer++;
+    Serial.println(alarmTimer);
+  }
+
+  if (digitalRead(buzzerPin) == LOW && alarmTimer == 60)
+  {
+    Blynk.notify("SECURITY ALARM STILL GOING: 60 SEC.");
+    Serial.println("SECURITY ALARM STILL GOING: 60 SEC.");
+  }
+  if (digitalRead(buzzerPin) == LOW && alarmTimer == 300)
+  {
+    Blynk.notify("SECURITY ALARM STILL GOING: 5 MIN.");
+    Serial.println("SECURITY ALARM STILL GOING: 5 MIN.");
+  }
+  if (digitalRead(buzzerPin) == LOW && alarmTimer == 600)
+  {
+    Blynk.notify("SECURITY ALARM STILL GOING: 10 MIN.");
+    Serial.println("SECURITY ALARM STILL GOING: 10 MIN.");
   }
 }
 
